eb6d2fbb2eba1aa155a279a700cf670ab721377f
Rajkumar-stackcoder <meet.r.graphicat10@gmail.com>
2021-10-14 11:23:50 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# PY Number Guessing Game
+
+
+![image](game.png)
+
+
+
+<br>
+
+<br>
+How to run game
+ 
+
+> python3 py-the-guessing.py
---
+++ py-the-guessing.py
@@ -0,0 +1,35 @@
+from colorama import Fore
+
+
+import random
+import math
+
+lower = int(input(Fore.BLUE + "Enter lower Range for Guessing:- "))
+
+
+higher = int(input(Fore.BLUE + "Enter Higher Range for Guessing:- "))
+
+
+x = random.randint(lower, higher)
+print("\n\tYou've only ",
+	round(math.log(higher - lower + 1, 2)), Fore.BLUE +
+	" chances to guess the integer!\n")
+
+count = 0
+
+
+while count < math.log(higher - lower + 1, 2):
+	count += 1
+	guess = int(input(Fore.BLUE + " Guess a number:- "))
+
+	# Condition testing
+	if x == guess:
+		print(Fore.BLUE + "Congratulations you did it in ",count, " try")
+		break
+	elif x > guess:
+		print(Fore.BLUE + "You guessed too small!")
+	elif x < guess:
+		print(Fore.BLUE + "You Guessed too high!")
+
+if count >= math.log(higher - lower + 1, 2):
+	print(Fore.BLUE + "\n The number is %d" % x)
